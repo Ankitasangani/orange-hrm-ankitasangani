1bc28545813bf5538173415b5d5dbcfd8a0ee9ca
Ankitasangani <166874980+Ankitasangani@users.noreply.github.com>
2024-06-18 20:27:34 +0100

    Update AdminPage.java

--- src/main/java/com/orangehrmlive/demo/pages/AdminPage.java
+++ src/main/java/com/orangehrmlive/demo/pages/AdminPage.java
@@ -6,6 +6,25 @@ import com.orangehrmlive.demo.utilities.Utility;
  * 4.AdminPage - UserManagement, Job, Organization Tabs Locators and it's actions
  */
 public class AdminPage extends Utility {
+ @CacheLookup
+    @FindBy(xpath = "//h5[normalize-space()='System Users']")
+    WebElement systemUsersText;
 
+    @CacheLookup
+    @FindBy(xpath = "//button[normalize-space()='Add']")
+    WebElement addButton;
+
+
+    public String getSystemUsersText() {
+        Reporter.log("get " + systemUsersText);
+        CustomListeners.test.log(Status.PASS, "get " + systemUsersText);
+        return getTextFromElement(systemUsersText);
+    }
+
+    public void clickOnAddButton() {
+        clickOnElement(addButton);
+        Reporter.log("click " + addButton);
+        CustomListeners.test.log(Status.PASS, "click " + addButton);
+    }
 
 }
