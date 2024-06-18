0a3cb2f2e1c72f2de811b36b7f06ac798e152272
Ankitasangani <166874980+Ankitasangani@users.noreply.github.com>
2024-06-18 20:36:43 +0100

    Update DashboardPage.java

--- src/main/java/com/orangehrmlive/demo/pages/DashboardPage.java
+++ src/main/java/com/orangehrmlive/demo/pages/DashboardPage.java
@@ -7,6 +7,24 @@ import com.orangehrmlive.demo.utilities.Utility;
  * and create appropriate methods for it.
  */
 public class DashboardPage extends Utility {
+ @CacheLookup
+    @FindBy(xpath = "//span[@class='oxd-userdropdown-tab']")
+    WebElement userProfileLogo;
 
+    @CacheLookup
+    @FindBy(xpath = "//a[normalize-space()='Logout']")
+    WebElement logoutButton;
+
+    public void clickOnUserProfileLogo() {
+        clickOnElement(userProfileLogo);
+        Reporter.log("click on " + userProfileLogo);
+        CustomListeners.test.log(Status.PASS, "click on " + userProfileLogo);
+    }
+
+    public void clickOnLogoutButton() {
+        clickOnElement(logoutButton);
+        Reporter.log("click on " + logoutButton);
+        CustomListeners.test.log(Status.PASS, "click on " + logoutButton);
+    }
 
 }
